b1d92453377ee392d1796899db3a463a4f1e6216
AnshPharate0106 <71764337+AnshPharate0106@users.noreply.github.com>
2020-10-21 20:55:47 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,19 @@
+class bob{
+    constructor(){
+        var options ={
+            
+            restitution :0.3,
+            friction :0.2,
+            density :1.2
+            
+        }
+        this.body = Bodies.circle(250,540,20,options);
+        this.width = 33;
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        fill("white");
+        rotate(angle);
+    }
+}
